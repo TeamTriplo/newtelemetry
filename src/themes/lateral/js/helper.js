2c90382912eb964fc0b5209dd57de5d75566559f
Tim Erickson <tim@triplo.co>
2020-09-11 17:56:45 -0500

    Removing some unnecessary contrib themes

--- src/themes/lateral/js/helper.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-  "use strict";
-  $(document).ready(function() {
-    $('.menu-toggle-button, .l-header a').click(function () {
-      $('.l-header').toggleClass('nav-visible');
-    });
-  });
-})(jQuery);
